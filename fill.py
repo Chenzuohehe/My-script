8c06587ea1a4db42de14711a198945e8fa5fde53
sscz_imac <chenzuo0730@outlook.com>
2018-06-08 16:10:48 +0800

    updata for wwdc

--- fill.py
+++ fill.py
@@ -7,24 +7,28 @@ import time
 # sleep
 
 # 目标苹果开发者账户然后创建证书，添加UDID，添加B ID，生成配置文件
+# 现在想做的是因为是不是会因为网页加载问题终端，我需要自动输出在哪一步中断，如何如何继续进行？
 
-
-
-
-appleID = "balunwp33702@163.com"
-password = ""
+appleID = "xxxxx@yeah.net"
+password = "xxxxx"
 CRSfilePath = "/Users/chenzuo/Desktop/CertificateSigningRequest.certSigningRequest"
-appName = "daodaoliehuo"
-aName = "ddlh"
+appName = "xxxxx"
+aName = "xxxx"
 bundleID = "com." + appName + "." + aName
-appChineseName = "刀刀烈火"
-udids = ["/Users/chenzuo/Desktop/udid.txt"]
+appChineseName = "xxx"
+udids = ["/Users/chenzuo/Desktop/松鼠测试人员udid.txt"]
 
 waitSec = 6
 
 driver = webdriver.Chrome()
 
+# browser = webdriver.Safari()
+# browser.get('http://weibo.com')
+
+
+# 登录开发者账号
 def login():
+    # 隐式等待 全局的
     driver.implicitly_wait(30)
     driver.get(
         "https://idmsa.apple.com/IDMSWebAuth/login?appIdKey=891bd3417a7776362562d2197f89480a8547b108fd934911bcbea0110d07f757&path=%2Faccount%2F&rv=1")
@@ -38,12 +42,14 @@ def login():
     # 自动登录OK
     time.sleep(waitSec)
 
+# 打开CIP
 def openCIP():
     driver.find_element_by_xpath("//*[@data-menu-option='cip']").click()
     # 找到CIP  Certificates, IDs & Profiles 点击
     time.sleep(waitSec)
 
-def addCertificates(index):
+# 创建证书 index: 0：开发证书 1：开发推送证书 2：生成发布证书 3：发布推送证书
+def ceartCeetificates(index):
     driver.find_element_by_xpath("//*[@data-reactid='.0.1.0.1.0.0.0.2.1:$0.$ProgramBlock_0.2.$0.$0.0']").click()
     time.sleep(waitSec)
     driver.find_element_by_xpath("//*[@class='toolbar-button add ']").click()
@@ -73,7 +79,7 @@ def addCertificates(index):
     # done
     time.sleep(waitSec)
 
-# def addDisCertificates():
+# 创建APP ID
 def creatAppId():
     driver.find_element_by_xpath("//*[@data-reactid='.0.1.0.1.0.0.0.2.1:$0.$ProgramBlock_2.2.$5']").click()
     time.sleep(waitSec)
@@ -92,10 +98,11 @@ def creatAppId():
     driver.find_element_by_xpath("//*[@class='button small right']").click()
     # done
 
+# 添加设备UDID
 def addDevices():
     # 因为设备不能重复注册，这边就默认是好的吧，实际使用的时候再细调
     for path in udids:
-        driver.find_element_by_xpath("//*[@data-reactid='.0.1.0.1.0.0.0.2.1:$0.$ProgramBlock_3.2.$12.$0.1']").click()
+        driver.find_element_by_xpath("//*[@data-link='/account/ios/device/']").click()
         time.sleep(waitSec)
         driver.find_element_by_xpath("//*[@class='toolbar-button add']").click()
         time.sleep(waitSec)
@@ -109,8 +116,9 @@ def addDevices():
         time.sleep(waitSec)
         driver.find_element_by_xpath("//*[@class='button small right navLink']").click()
 
+# 生成配置文件 index 0：开发配置文件 1：hoc配置文件 2：生产配置文件
 def addProfiles(index):
-    driver.find_element_by_xpath("//*[@data-reactid='.0.1.0.1.0.0.0.2.1:$0.$ProgramBlock_4.2.$18']").click()
+    driver.find_element_by_xpath("//*[@data-link='/account/ios/profile/']").click()
     time.sleep(waitSec)
     driver.find_element_by_xpath("//*[@class='toolbar-button add']").click()
     time.sleep(waitSec)
@@ -156,17 +164,20 @@ def addProfiles(index):
     time.sleep(waitSec)
 
 
+# 打开iTunesConnect
 def openiTunesConnect():
     time.sleep(waitSec)
     driver.find_element_by_xpath("//*[@data-menu-option='itunes-connect']").click()
     time.sleep(waitSec)
     driver.find_element_by_xpath("//*[@class='button-3d-blue font-semibold']").click()
 
+#
 def openiTunesType(index):
     time.sleep(waitSec)
     driver.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa/ra/ng/app")
     time.sleep(waitSec)
 
+# 添加新APP
 def addNewApp():
     time.sleep(waitSec)
 
@@ -193,21 +204,26 @@ def addNewApp():
     driver.find_element_by_xpath("//*[@ng-click='saveApp()']").click()
     time.sleep(waitSec*1.5)
 
-
 if __name__ == '__main__':
     login()
-    # openCIP()
+    openCIP()
+    #
     # creatAppId() #生成appid
-    # addCertificates(0)#生成开发证书
-    # addCertificates(1)  # 生成开发推送证书
-    # addCertificates(2)#生成发布推送证书
-    # addCertificates(3)  # 生成发布推送证书
+    #
+    # ceartCeetificates(0)#生成开发证书
+    # ceartCeetificates(2)#生成发布证书
+    #
+    # # ceartCeetificates(1)  # 生成开发推送证书
+    # # ceartCeetificates(3)  # 生成发布推送证书
+    # #
     # addDevices() #根据txt地址添加UDID
-    # addProfiles(0) #生成开发配置文件
-    # addProfiles(2) #生成生产配置文件
+    #
+    addProfiles(0) #生成开发配置文件
+    addProfiles(2) #生成生产配置文件
 
     # openiTunesConnect()
-    # addNewApp() #创建新APP
+    # addNewApp()
+
 
 
 
